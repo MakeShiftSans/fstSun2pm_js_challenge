487f31f3246567216be028e769cbc6019ce7e9ab
Hassan Daahir <user@users-MacBook-Pro.local>
2023-10-08 14:50:43 +0100

    first commit

---
+++ loop.js
@@ -0,0 +1,16 @@
+// write a for loop that prints your name 10 times without using code snippets 
+
+
+for(let i = 0; i<10;i++){
+    console.log('Hassan')
+}
+
+
+// write a while loop that prints your surname 20 times
+
+let counter = 0
+
+while(counter < 20){
+    console.log('Daahir')
+    counter++;
+}
